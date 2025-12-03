12bc6b148b1cf6a4785c5ccf4641cef6833c650c
Vedant Ajay Shivsharan <vedantshivsharan14@gmail.com>
2025-12-03 14:21:22 +0530

    Clean up header.h by removing commented includes

--- header.h
+++ header.h
@@ -1,12 +1,3 @@
-// #include<stdio.h>
-// #include<stdlib.h>
-// #include<string.h>
-
-// int check_mp3_file(char *file_name);
-// int valid_header(char *file_name);
-// void view_song_details(char *file_name);
-// void show_help();
-// void edit_mp3_tags(char *filename);
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -21,4 +12,5 @@ void show_edit_menu();
 
 // Conversion functions
 unsigned int big_to_little_endian(unsigned char bytes[4]);
+
 void little_to_big_endian(unsigned int size, unsigned char result[4]);
